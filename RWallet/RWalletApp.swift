0a610c801a2f33bf07ca752b3a2e6f8ea807f71f
0xmwehehe <dawns.pregame_0f@icloud.com>
2025-12-24 06:07:46 +0700

    Initial Commit

---
+++ RWallet/RWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWalletApp.swift
+//  RWallet
+//
+//  Created by Rendi  on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
